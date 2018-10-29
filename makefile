dc2a091171ef210cacd16fb5848e987286103c65
perjnilsson <per90nilsson@gmail.com>
2018-10-29 17:56:29 +0100

    Fully working with arguments

--- makefile
+++ makefile
@@ -1,16 +1,30 @@
 .PHONY : all
 
-EXECS= mpi_djik run
+EXECS= mpi_djik run1
 MPICC?=mpicc
 
 
 all : ${EXECS}
 
-mpi_djik : mpi_djik
-	$(MPICC) -o mpi_djik_exe mpi.c
+mpi_djik :
+	$(MPICC) -o djikstra mpi.c -lm
 
-run : run
-	@time mpirun -n 4 ./mpi_djik_exe
+run1 : 
+	time -p mpirun -n 1 ./djikstra 9 82 /home/hpc2018/a5_grading/test_data/graph_de1_ne3_we2 
+
+run2 : 
+	time -p mpirun -n 1 ./djikstra 17 40 /home/hpc2018/a5_grading/test_data/graph_de2_ne4_we2 
+
+run3 : 
+	time -p mpirun -n 4 ./djikstra 17 18 /home/hpc2018/a5_grading/test_data/graph_de2_ne4_we2 
+
+run4 : 
+	time -p mpirun -n 10 ./djikstra 107 0 /home/hpc2018/a5_grading/test_data/graph_de2_ne5_we2 
+
+run5 : 
+	time -p mpirun -n 20 ./djikstra 4 5 /home/hpc2018/a5_grading/test_data/graph_de3_ne5_we3 
+
+
 
 clean:
 	rm ${EXECS}
--- mpi.c
+++ mpi.c
@@ -19,7 +19,20 @@ int processID;
 int dijkstra(int startNode, int endNode);
 
 int main(int argc, char** argv) {
+  
+  int start_node = strtol(argv[1], NULL, 10);
+  int end_node = strtol(argv[2], NULL, 10);
+  char * filename = argv[3];
 
+  
+  int length = strlen(filename);
+  int degree_exp = filename[length-9] - '0';
+  int nodes_exp = filename[length-5] - '0';
+  int weights_exp = filename[length-1] - '0';
+  degrees = pow(10, degree_exp);
+  numberOfNodes = pow(10, nodes_exp);
+
+  
   // ***** Initializing the MPI ***** //
   MPI_Init(NULL, NULL);
 
@@ -28,42 +41,23 @@ int main(int argc, char** argv) {
 
   // Get the rank of the process
   MPI_Comm_rank(MPI_COMM_WORLD, &processID);
-
-  for (int i=0; i<numberOfProcesses; i+=numberOfProcesses) {
-    printf("ProcessId: %d\n", processID);
-  }
   
-  degrees = 10;
-  numberOfNodes = 1000;
-
-  int start_node = strtol(argv[4], NULL, 10);
-  int end_node = strtol(argv[5], NULL, 10);
-
-  char * filename = argv[6];
-     
-  FILE * fp;
-  fp = fopen(filename, "r");
-  
-  int length = strlen(filename);
-  int degree_exp = filename[length-9] - '0';
-  int nodes_exp = filename[length-5] - '0';
-  int weights_exp = filename[length-1] - '0';
-
-
-  degrees = pow(10, degree_exp);
-  numberOfNodes = pow(10, nodes_exp);
   numberOfEdges = numberOfNodes*degrees;
   unvisitedNodes =  calloc(numberOfNodes, sizeof(int));
   weights = malloc(sizeof(int)*numberOfEdges);
   valueOfNodes = malloc(sizeof(int)*numberOfNodes);
  
-  
+  //printf("degrees: %d \nnodes: %d\nedges: %d\n",degrees, numberOfNodes, numberOfEdges);
+
   graphRows = malloc(sizeof(int)*2*numberOfEdges);
   graph = malloc(sizeof(int*)*numberOfEdges);
   for (size_t i=0, j=0; i<numberOfEdges; i++, j+=2) {
     graph[i] = graphRows + j;
-  }
+}
 
+
+  FILE * fp;
+  fp = fopen(filename, "r"); // ("/home/hpc2018/a5_grading/test_data/graph_de1_ne3_we2"
   for (int i=0; i<numberOfEdges; i++) {
     int a,b,c;
     fscanf(fp, "%d %d %d", &a, &b, &c);
@@ -74,11 +68,15 @@ int main(int argc, char** argv) {
   fclose(fp);
 
   int shortestPath;
-  shortestPath = dijkstra(9, 82);
+  shortestPath = dijkstra(start_node, end_node);
 
-  printf("The shortest path is: %d\n", shortestPath);
-    // Finalize the MPI environment.
-    MPI_Finalize();
+  if (processID == 0) {
+    printf("The shortest path is: %d\n", shortestPath);
+  }
+
+  // Finalize the MPI environment.
+  MPI_Finalize();
+
 }
 // Function for binary search if not all nodes are evenly distributed
 int binarySearchFun(int currentNode) {
@@ -113,6 +111,7 @@ int binarySearchFun(int currentNode) {
 int dijkstra(int startNode, int endNode) {
   int largeTenativeInt = INFINITY_LENGTH;
   int currentNode;
+  int tmpNode;
   for (int i=0; i<numberOfNodes; i++) {
     valueOfNodes[i] = largeTenativeInt;
   }
@@ -120,48 +119,54 @@ int dijkstra(int startNode, int endNode) {
   valueOfNodes[startNode] = 0;
   currentNode = startNode;
   int pair[2];
-  int tmp_pair[2] = {0, 0};
-    
+  int tmp_pair[2];
+  int counter =1;
   while (1) {
     for (int i = currentNode*degrees; i<currentNode*degrees+degrees; i++) {
-      // if (i%comm_size != rank) continue;    
       if (unvisitedNodes[graph[i][1]] == 0){
 	int tmpNodeValue = valueOfNodes[currentNode] + weights[i]; // Put it in two if-cases to make it clearer. Does not make any runtime difference
 	if (tmpNodeValue < valueOfNodes[graph[i][1]]) {
 	  valueOfNodes[graph[i][1]] = valueOfNodes[currentNode] + weights[i];
 	}
       }
-   
     }
-    unvisitedNodes[currentNode] = 1;
-
+    
     int tmpLowest = INFINITY_LENGTH;
     for (int ix=processID; ix< numberOfNodes; ix+=numberOfProcesses) {
       if ( unvisitedNodes[ix] == 0 && valueOfNodes[ix] < tmpLowest) {
 	tmpLowest = valueOfNodes[ix]; // save the temporary lowest value
-	currentNode = ix; // saves the node with the temporary lowest value,
+	tmpNode = ix; // saves the node with the temporary lowest value,
       }
     }
     pair[0] = tmpLowest; // save the lowest value form each iteration
-    pair[1] = currentNode; // saves the current node with the lowest value,
-    
+    pair[1] = tmpNode; // saves the current node with the lowest value,
+
     MPI_Status status;
+  
     if (processID != 0) {
-      MPI_Send(pair, 2, MPI_INT, 0, 1, MPI_COMM_WORLD);
+      MPI_Send(pair, 2, MPI_INT, 0, 1, MPI_COMM_WORLD); /* Each MPI node has different tmpLowest and tmpNode. Now need to send to master to find
+                                                           which one we should pick of all the MPI workers */
     }else {
       for (int k=1; k<numberOfProcesses; k++) {
 	MPI_Recv(tmp_pair, 2, MPI_INT,k, 1, MPI_COMM_WORLD, &status);
-	if (tmp_pair[0] < pair[1]) {
+	if (tmp_pair[0] < pair[0]) {
 	  pair[0] = tmp_pair[0];
 	  pair[1] = tmp_pair[1];
-	}
+        }
       }
     }
+    MPI_Bcast(pair, 2, MPI_INT, 0, MPI_COMM_WORLD);
+    
+    unvisitedNodes[currentNode] = 1;
+    currentNode = pair[1];
+    valueOfNodes[currentNode]= pair[0];
+    
     if (unvisitedNodes[endNode] == 1) {
       return valueOfNodes[endNode];
-      }
-    if ( tmpLowest == INFINITY_LENGTH) {
+    }
+    if (counter == numberOfEdges*numberOfEdges) { // If need to check n*n for every iteration, worst case scenario
       return INFINITY_LENGTH;
     }
+    counter++;
   }
 }   // End Djikstra funtion
